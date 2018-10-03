1dbd3f212b6e3b774839c566213824b11504f654
Christopherlam23 <christopherlam@live.com>
2018-10-03 22:33:19 +0400

    Profiles Component  getProfiles Action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,7 +4,8 @@ import {
   PROFILE_LOADING,
   GET_ERRORS,
   CLEAR_CURRENT_PROFILE,
-  SET_CURRENT_USER
+  SET_CURRENT_USER,
+  GET_PROFILES
 } from './types';
 
 // GET current profile
@@ -121,6 +122,25 @@ export const deleteAccount = () => dispatch => {
         })
       );
   }
+};
+
+// GET all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get('/api/profile/all')
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
+      })
+    );
 };
 
 // Profile Loading
