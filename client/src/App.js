9180e5f3e268108d5b8f3d98580faf70070a1572
Christopherlam23 <christopherlam@live.com>
2018-10-10 21:38:20 +0400

    Add/Delete/Like/Unlike Posts/Comments

--- client/src/App.js
+++ client/src/App.js
@@ -28,6 +28,9 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import NotFound from './components/not-found/NotFound';
 
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
+
 import './App.css';
 
 // check for token
@@ -101,6 +104,12 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/post/:id" component={Post} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
