61149cfeccd0ce5c8f32897a540ec7cba49d74b6
Christopherlam23 <christopherlam@live.com>
2018-10-17 22:17:05 +0400

    Bug correction [webapp does not load if redux DevTools is not an extension in browser] v2

--- client/src/store.js
+++ client/src/store.js
@@ -4,13 +4,10 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
-const initialState = {};
-
 const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
-  initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
